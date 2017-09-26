6021e539de57317e4e835dcde0fe05e9c950a472
fsaibene <fernandomsaibene@hotmail.com>
2017-09-26 00:25:27 -0300

    primer comm, login register ok

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCYVERzZYpwCLi2OF7RJ7GtRHTOnPNDW2s",
+  authDomain: "conversandoenelaula-b9f77.firebaseapp.com",
+  databaseURL: "https://conversandoenelaula-b9f77.firebaseio.com",
+  projectId: "conversandoenelaula-b9f77",
+  storageBucket: "",
+  messagingSenderId: "156831619581"
+};
